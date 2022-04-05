a260ea87588509cf0a3e0fc281a9a203d9dc3ae5
Eric Toreki <etoreki@udel.edu>
2022-04-05 15:38:54 -0400

    fixed pretteir problem with name

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Final Project done by: Eric Toreki
-            </p>
+            <p>Final Project done by: Eric Toreki</p>
         </div>
     );
 }
