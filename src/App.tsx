336cee9ebd97c7e7e021db77b05803e139045020
Eric Toreki <etoreki@udel.edu>
2022-04-05 15:37:47 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                Final Project done by: Eric Toreki
             </p>
         </div>
     );
