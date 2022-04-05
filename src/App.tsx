ef6ff059cccc8836f754d2d0f0b89b44f01e73bf
Maxwell Wang <maxwang@udel.edu>
2022-04-05 15:42:14 -0400

    my name is Max!

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                My name is Maxwell Wang
             </p>
         </div>
     );
