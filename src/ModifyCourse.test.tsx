305c90c5085b732f1cf54e9460dbf0b50759fa1e
Maxwell Wang <maxwang@udel.edu>
2022-04-07 19:05:23 -0400

    prereq is now a string

--- src/ModifyCourse.test.tsx
+++ src/ModifyCourse.test.tsx
@@ -3,7 +3,12 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 
 describe("Modify Course.test", () => {
-    render(<ModifyCourse />);
+    beforeEach(() => {
+        render(<ModifyCourse />);
+    });
+    
+    test("CISC-108 is shown", () => {
+        const CISC108 = screen.getByText(/ModifyCourse: (.*)/i);
+        expect(CISC108).toBeInTheDocument();
+    });
 });
-
-test("CISC-108 is shown");
