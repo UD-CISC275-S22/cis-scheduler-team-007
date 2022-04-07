1219913e018f5cc4dfe7a42c0d619779ad8bcae5
Eric Toreki <etoreki@udel.edu>
2022-04-07 14:51:08 -0400

    Displays list of plans

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import "./App.css";
 import { Plan } from "./Planner-Interfaces/plan";
 
 function App(): JSX.Element {
-    const [degreePlans, setDegreePlans] = useState<Plan[]>([]);
+    const [degreePlans, setDegreePlans] = useState<Plan[]>([
+        { id: "1", name: "Test1", semester: [], requiredCourses: [] },
+        { id: "2", name: "Test2", semester: [], requiredCourses: [] }
+    ]);
     return (
         <div className="App">
             <header className="App-header">UD CISC Degree Planner</header>
             <p>By: Eric Toreki, Maxwell Wang, Joshua Strassle</p>
+            <li>
+                {degreePlans.map((plan: Plan) => (
+                    <ul key={plan.id}>{plan.name}</ul>
+                ))}
+            </li>
         </div>
     );
 }
