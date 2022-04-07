abdc23970cc2d1bb45ad9b39d5f71148c06c1e86
Maxwell Wang <maxwang@udel.edu>
2022-04-07 13:27:39 -0400

    made a deep copy from the ciscCourses.json

---
+++ src/ModifyCourse.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Course } from "./Planner-Interfaces/course";
+import Courses from "./CISC-Courses-data/ciscCourses.json";
+
+const compSciCourses = Courses.map(
+    (course): Course => ({
+        ...course,
+        id: course.id,
+        name: course.name,
+        courseId: course.courseId,
+        prereq: course.preReq
+    })
+);
+
+export function ModifyCourse(): JSX.Element {
+    const [course, setCourse] = useState<Course[]>();
+
+    function deleteCourse(id: string) {
+        setCourse(
+            compSciCourses.filter((course: Course): boolean => course.id !== id)
+        );
+    }
+    return <div></div>;
+}
