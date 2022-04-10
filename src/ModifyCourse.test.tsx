a3e85451296ec6689455a9c53c41fd4dcf065a0d
Maxwell Wang <maxwang@udel.edu>
2022-04-09 22:35:25 -0400

    no linter errors atm

--- src/ModifyCourse.test.tsx
+++ src/ModifyCourse.test.tsx
@@ -1,5 +1,5 @@
 import { ModifyCourse } from "./ModifyCourse";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import React from "react";
 
 describe("Modify Course.test", () => {
