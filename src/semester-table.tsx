2c74aa5ec81454b3d56dcc3872effef564e16b2d
Maxwell Wang <maxwang@udel.edu>
2022-04-12 12:08:01 -0400

    commented out some imports

---
+++ src/semester-table.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+//import { Button, Table } from "react-bootstrap";
+import { Semester } from "./Planner-Interfaces/semester";
+import { Course } from "./Planner-Interfaces/course";
+//import { Semester } from "./Planner-Interfaces/semester";
+
+interface thisCourse {
+    course: Course;
+    remove_course: (num: number, str: string) => void;
+}
+interface thisSemester {
+    courses: Course[];
+    semesterId: number;
+    semList: Semester[];
+    update: (semester: Semester[]) => void;
+}
+
+function display_course({ course, remove_course }: thisCourse) {
+    return (
+        <tr>
+            <td>
+                <b>{course.name}:</b>
+                {course.courseId}
+            </td>
+            <td>{course.credits}</td>
+            <td>
+                <button
+                    onClick={() =>
+                        remove_course(course.courseId as number, course.name)
+                    }
+                >
+                    Remove Course
+                </button>
+            </td>
+        </tr>
+    );
+}
+export function displaySemester({
+    courses,
+    semesterId,
+    semList,
+    update
+}: thisSemester) {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Courses</th>
+                        <th>Credits</th>
+                        <th>Edit Course</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courses.map((course: Course, listNum: number) => {
+                        return course;
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
