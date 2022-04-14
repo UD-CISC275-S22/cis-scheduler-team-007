5688f643507eaafbdf6b518388697a6ad2e6db39
Joshu Strassle <j.strassle@icloud.com>
2022-04-14 14:08:05 -0400

    updated semester table

--- src/semester-table.tsx
+++ src/semester-table.tsx
@@ -1,59 +1,31 @@
 import React from "react";
-//import { Button, Table } from "react-bootstrap";
-import { Semester } from "./Planner-Interfaces/semester";
 import { Course } from "./Planner-Interfaces/course";
-//import { Semester } from "./Planner-Interfaces/semester";
-/*
-interface thisCourse {
-    course: Course;
-    remove_course: (num: number, str: string) => void;
-}
-*/
-interface thisSemester {
-    courses: Course[];
-    semesterId: number;
-    semList: Semester[];
-    update: (semester: Semester[]) => void;
-}
-/*
-function display_course({ course, remove_course }: thisCourse) {
+import { Semester } from "./Planner-Interfaces/semester";
+
+export function SemesterTable({
+    currentSemester
+}: {
+    currentSemester: Semester;
+}): JSX.Element {
     return (
-        <tr>
-            <td>
-                <b>{course.name}:</b>
-                {course.courseId}
-            </td>
-            <td>{course.credits}</td>
-            <td>
-                <button
-                    onClick={() =>
-                        remove_course(course.courseId as number, course.name)
-                    }
-                >
-                    Remove Course
-                </button>
-            </td>
-        </tr>
-    );
-}
-*/
-export function displaySemester({ courses }: thisSemester) {
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Courses</th>
-                        <th>Credits</th>
-                        <th>Edit Course</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {courses.map((course: Course) => {
-                        return course;
-                    })}
-                </tbody>
-            </table>
-        </div>
+        <table className="Table-Header">
+            <tbody>
+                <tr>
+                    <th>Course</th>
+                    <th>Title</th>
+                    <th>Credits</th>
+                    <th>Description</th>
+                </tr>
+                {currentSemester.courses.map((course: Course) => {
+                    return (
+                        <tr key={course.courseId}>
+                            <td>{course.courseId}</td>
+                            <td>{course.name}</td>
+                            <td>{course.credits}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
     );
 }
