4503e7bd92fc3b8caf2377fc578f811b4a4a5c87
Maxwell Wang <maxwang@udel.edu>
2022-04-16 08:58:14 -0400

    implemented tests to test the hidden message

--- src/Messaging.test.tsx
+++ src/Messaging.test.tsx
@@ -1,12 +1,22 @@
 import { Messaging } from "./Messaging";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
 describe("Messaging Test", () => {
     beforeEach(() => {
         render(<Messaging></Messaging>);
     });
-    test("There is a message displayed on the webpage", () => {
+    test("The message is initially hidden", () => {
+        const valueText = screen.queryByText(
+            /Hello CISC or INSY majors and minors/i
+        );
+        expect(valueText).toBeNull();
+    });
+    test("There is a message displayed on the webpage after button is clicked", () => {
+        const messaging = screen.getByRole("button", {
+            name: /Display Message/i
+        });
+        messaging.click();
         expect("Hello CISC or INSY majors and minors").toBeInTheDocument;
         expect("In here you are choosing a plan for your cisc degree")
             .toBeInTheDocument;
