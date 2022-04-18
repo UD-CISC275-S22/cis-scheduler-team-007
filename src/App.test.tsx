f37bf256dacd8f657b3b0c4ea84973cc8c88fa3a
Eric Toreki <etoreki@udel.edu>
2022-04-18 19:50:46 -0400

    Updated tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,34 @@ test("renders the title name somewhere", () => {
     const linkElement = screen.getByText(/UD CISC Degree Planner/i);
     expect(linkElement).toBeInTheDocument();
 });
+
+describe("App Component Tests", () => {
+    beforeEach(() => {
+        render(<App></App>);
+    });
+    test("No Plan Seleccted by default", () => {
+        expect(screen.getByText("No Plan Selected")).toBeInTheDocument();
+    });
+    test("Delete and Add Plan button exist", () => {
+        expect(
+            screen.getByRole("button", { name: /Add new Plan/i })
+        ).toBeInTheDocument();
+        expect(
+            screen.getByRole("button", { name: /Delete Selected Plan/i })
+        ).toBeInTheDocument();
+    });
+    test("Add Plan adds plan and switches to new plan", () => {
+        const addButton = screen.getByRole("button", { name: /Add new Plan/i });
+        addButton.click();
+        expect(screen.getByText("New Plan")).toBeInTheDocument();
+    });
+    test("Delete Plan removes current plan and switches to no plan", () => {
+        const addButton = screen.getByRole("button", { name: /Add new Plan/i });
+        addButton.click();
+        const delButton = screen.getByRole("button", {
+            name: /Delete Selected Plan/i
+        });
+        delButton.click();
+        expect(screen.getByText("No Plan Selected")).toBeInTheDocument();
+    });
+});
