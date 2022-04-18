e22708210fefe52e5d9cbb0b01b7f32577c70f6f
Joshu Strassle <j.strassle@icloud.com>
2022-04-18 10:38:39 -0400

    changed table to remove description

--- src/semester-table.tsx
+++ src/semester-table.tsx
@@ -8,24 +8,26 @@ export function SemesterTable({
     currentSemester: Semester;
 }): JSX.Element {
     return (
-        <table className="Table-Header">
-            <tbody>
-                <tr>
-                    <th>Course</th>
-                    <th>Title</th>
-                    <th>Credits</th>
-                    <th>Description</th>
-                </tr>
-                {currentSemester.courses.map((course: Course) => {
-                    return (
-                        <tr key={course.courseId}>
-                            <td>{course.courseId}</td>
-                            <td>{course.name}</td>
-                            <td>{course.credits}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+        <div>
+            <head></head>
+            <table className="Table-Header">
+                <tbody>
+                    <tr>
+                        <th>Course</th>
+                        <th>Title</th>
+                        <th>Credits</th>
+                    </tr>
+                    {currentSemester.courses.map((course: Course) => {
+                        return (
+                            <tr key={course.courseId}>
+                                <td>{course.courseId}</td>
+                                <td>{course.name}</td>
+                                <td>{course.credits}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 }
