ea3135e5a98c39c1eb2a1e36fbf1def6e285cb32
Eric Toreki <etoreki@udel.edu>
2022-05-17 19:48:49 -0400

    Comments added to code

--- src/PreReqs.tsx
+++ src/PreReqs.tsx
@@ -9,8 +9,10 @@ export function PreReqs({
     allSemesters: Semester[];
     semester: Semester;
 }): JSX.Element {
-    const failedPreReqs: string[] = [];
+    const failedPreReqs: string[] = []; //All the messages of prereqs that are not met in the semester
+    //Checks a single course prereq to see if it is fullfilled
     function checkCoursePreReq(course: Course) {
+        //If course has no prereq just return
         if (course.preReq === "") {
             return;
         }
@@ -18,26 +20,31 @@ export function PreReqs({
         let foundPreReq = false;
         allSemesters.forEach((sem: Semester) => {
             if (sem.id === semester.id) {
-                pastCurrentSem = true;
+                pastCurrentSem = true; //Set to true once you are at your semester as only semester prior to your semester can fullfill prereq
             }
             if (pastCurrentSem) {
+                //Past or at your semester so don't check
                 return;
             }
+            //Check each courseId in the semester to see if it matches the prereq
             sem.courses.forEach((prevCourse: Course) => {
                 if (course.preReq.search(prevCourse.courseId) !== -1) {
-                    foundPreReq = true;
+                    foundPreReq = true; //Becomes true if courseID matched the prereq
                 }
             });
         });
+        //If prereq wasn't found add it to the list
         if (!foundPreReq) {
             failedPreReqs.push(course.courseId + " requires " + course.preReq);
         }
     }
+    //Go through each course in the semester and see if prereqs are met
     semester.courses.forEach((curCourse: Course) =>
         checkCoursePreReq(curCourse)
     );
     return (
         <div>
+            {/*Return all missing prereq messages*/}
             {failedPreReqs.map((missingReq: string) => (
                 <p key={missingReq}>{missingReq}</p>
             ))}
