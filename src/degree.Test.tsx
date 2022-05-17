12aaa380174b900fac004073b1bc87cfbc84bdc0
Maxwell Wang <maxwang@udel.edu>
2022-05-17 10:25:40 -0400

    drafted a list of semesters with some courses

---
+++ src/degree.Test.tsx
@@ -0,0 +1,86 @@
+import { DegreeRequirements_Section } from "./degree";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Modal } from "react-bootstrap";
+
+describe("Degree_Requirements_Section test", () => {
+    beforeEach(() => {
+        render(
+            <DegreeRequirements_Section
+                show={true}
+                setShow={function (b: boolean): void {
+                    throw new Error("Function not implemented.");
+                }}
+                userSemesters={[
+                    {
+                        season: "Fall",
+                        year: 2019,
+                        name: "My Fall Semester",
+                        id: "F19",
+                        courses: [
+                            {
+                                name: "Introduction to Computer Science I",
+                                credits: 3,
+                                courseId: "CISC-108",
+                                preReq: "",
+                                id: ""
+                            },
+                            {
+                                name: "English in Composition",
+                                credits: 3,
+                                courseId: "ENGL-110",
+                                preReq: "",
+                                id: ""
+                            },
+                            {
+                                name: "Introduction to Engineering",
+                                credits: 3,
+                                courseId: "EGGG-101",
+                                preReq: "",
+                                id: ""
+                            },
+                            {
+                                name: "Calculus 1",
+                                credits: 3,
+                                courseId: "Math-241",
+                                preReq: "",
+                                id: ""
+                            }
+                        ]
+                    },
+                    {
+                        season: "Spring",
+                        year: 2020,
+                        name: "My Spring Semester",
+                        id: "S20",
+                        courses: [
+                            {
+                                name: "Introduction to Computer Science II",
+                                credits: 3,
+                                courseId: "CISC-181",
+                                preReq: "CISC-108 or CISC-106",
+                                id: ""
+                            }
+                        ]
+                    }
+                ]}
+            ></DegreeRequirements_Section>
+        );
+        test("Modal shows up", () => {
+            const button = screen.getByText("button");
+            expect(button).toBeInTheDocument();
+            button.click();
+        });
+        test("University Requirements are shown", () => {
+            expect(screen.getByText("University Requirements"));
+        });
+        test("Modal closes when closed", () => {
+            const handleClose = jest.fn();
+            const { getByText } = render(
+                <Modal onClose={handleClose}>Degree Requirements</Modal>
+            );
+            expect(getByText("Degree Requirements")).toBeTruthy();
+            expect(handleClose).toHaveBeenCalledTimes(1);
+        });
+    });
+});
